cb2942e7ecac04161d4751c84aff6773e61d8121
Borededdy <83037613+Borededdy@users.noreply.github.com>
2023-03-01 09:22:29 +0100

    Added new version of JavIN

--- JavIN.java
+++ JavIN.java
@@ -1,17 +1,23 @@
-package //!! Insert the name of your package here !!;
+package javin;
 
 //Imports
 import java.util.Scanner;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
 
 public class JavIN {
     //Making of Objects
-    Scanner sc = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
+    static Date date = new Date();
+    //The Date format is the US Standard one, change it if you need to.
+    static SimpleDateFormat SDF = new SimpleDateFormat("MM-dd-yyyy");
     
     //Default constructor
     public JavIN (){}
     
     //Input method for INT Var
-    public int inINT(){
+    public static int inINT(){
         int data = 0;
         try{
             data = sc.nextInt();
@@ -22,7 +28,7 @@ public class JavIN {
     }
     
     //Input method for DOUBLE Var
-    public double inDOUBLE(){
+    public static double inDOUBLE(){
         double data = 0.0;
         try{
             data = sc.nextDouble();
@@ -33,7 +39,7 @@ public class JavIN {
     }
     
     //Input method for FLOAT Var
-    public float inFLOAT(){
+    public static float inFLOAT(){
         float data = 0;
         try{
             data = sc.nextFloat();
@@ -44,16 +50,27 @@ public class JavIN {
     }
     
     //Input method for STRING Var
-    public String inSTR(){
+    public static String inSTR(){
         String data;
         data = sc.nextLine();
         return data;
     }
     
     //Input method for BOOLEAN Var
-    public boolean inBOOL(){
+    public static boolean inBOOL(){
         boolean data;
         data = sc.nextBoolean();
         return data;
+    }
+    
+    //Input metod for DATE
+    public static String inDATE(){
+        String dataIN = sc.nextLine();
+        try{
+            date = SDF.parse(dataIN);
+        }catch(ParseException ex){
+            System.err.println("Error 104: Bad date format. For more info check wiki");
+        }
+        return dataIN;
     }
 }
